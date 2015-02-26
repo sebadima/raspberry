30ff767f68603555c89bcaadb444e036497de689
Raspberry <raspberry@sebadima.com>
2015-02-26 15:12:42 +0000

    modif

--- test.pl
+++ test.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl -w
 #!/usr/local/bin/perl
 print "Content-type: text/html\n\n";
-print "<H1> Il led dovrebbe essere ACCESSO, adesso</H1>\n";
+#print "<H1> Il led dovrebbe essere ACCESSO!</H1>\n";
 
-
-print "<form> <input type=\"button\" value=\"Acceso! Torna Indietro\"back\" onClick=\"javascript:history.back(1)\"> </form>";
+print "<form> <input type=\"button\" value=\"Torna Indietro\"back\"  onClick=\"javascript:history.back(1)\"> </form>";
 exec("sudo ./test.sh");
+
--- test2.pl
+++ test2.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
 #!/usr/local/bin/perl
 print "Content-type: text/html\n\n";
-print "<form> <input type=\"button\" value=\"Spento... Torna Indietro\"back\" onClick=\"javascript:history.back(1)\"> </form>";
+print "<form> <input type=\"button\" value=\"Torna Indietro\"back\" onClick=\"javascript:history.back(1)\"> </form>";
 exec("sudo ./test2.sh");
